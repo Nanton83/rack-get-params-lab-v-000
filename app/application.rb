46b60bdf7240c176816b318a39814fbaefc4ef5d
Nick Romans <nicholas.romans@gmail.com>
2019-01-16 21:21:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -31,7 +31,7 @@ class Application
           resp.write "added #{"item_to_add"}"
         else
           resp.write "We don't have that item"
-      end
+      
     end
 
     elsif req.path.match(/search/)
